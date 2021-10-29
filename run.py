40a80a14c2278cd43b09dd355b2305ccb677adc6
Ronak Mehta <ronakrm@cs.wisc.edu>
2021-10-29 11:05:01 -0500

    run file big update

--- run.py
+++ run.py
@@ -7,9 +7,9 @@ import torchvision
 
 from torch_src.nn_utils import manual_seed, do_reg_epoch
 
-from torch_src.datasets import MNISTF1
-from torch_src.models.mnist import FFNetF1
-from demd import DEMDFairLoss
+from torch_src.datasets import BinarySizeMNIST
+from torch_src.models.mnist import BinMNISTNET
+from demd import DEMDLayer
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -18,32 +18,33 @@ def main(args):
 	outString = 'trained_models/'+args.dataset+"_"+args.model+'_epochs_' + str(args.epochs)+'_lr_' + str(args.learning_rate)+'_wd_' + str(args.weight_decay)+'_bs_' + str(args.batch_size)+'_optim_' + str(args.optim)
 	print(outString)
 	
-	model = FFNetF1()
-	criterion = torch.nn.CrossEntropyLoss()
-	# reg = DEMDFairLoss()
+	model = BinMNISTNET(num_classes=1)
+	# criterion = torch.nn.CrossEntropyLoss()
+	criterion = torch.nn.BCELoss()
+	reg = DEMDLayer()
 	print('Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
 
 	optim = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
 
-	train_dataset = MNISTF1('.data/', train=True)
-	valid_dataset = MNISTF1('.data/', train=True)
+	train_dataset = BinarySizeMNIST('./data/', train=True, download=True)
+	valid_dataset = BinarySizeMNIST('./data/', train=False, download=True)
 	print(len(train_dataset))
-	print(len(val_dataset))
+	print(len(valid_dataset))
 
 	train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size,
 											  shuffle=True, num_workers=1)
 
-	val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size,
+	valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size,
 											 shuffle=False, num_workers=1)
 
 	for epoch in range(args.epochs):
 		train_loss, train_accuracy = do_reg_epoch(model, train_loader, criterion, reg, epoch, args.epochs, optim=optim, device=device, outString=outString)
 
 		with torch.no_grad():
-			val_loss, val_accuracy = do_reg_epoch(model, val_loader, criterion, reg, epoch, args.epochs, optim=None, device=device, outString=outString)
+			valid_loss, valid_accuracy = do_reg_epoch(model, valid_loader, criterion, reg, epoch, args.epochs, optim=None, device=device, outString=outString)
 
 		tqdm.write(f'{args.model} EPOCH {epoch:03d}: train_loss={train_loss:.4f}, train_accuracy={train_accuracy:.4f} '
-				   f'val_loss={val_loss:.4f}, val_accuracy={val_accuracy:.4f}')
+				   f'valid_loss={valid_loss:.4f}, valid_accuracy={valid_accuracy:.4f}')
 
 		print('Saving model...')
 		torch.save(model.state_dict(), outString + '.pt')
@@ -51,8 +52,8 @@ def main(args):
 if __name__ == '__main__':
 	arg_parser = argparse.ArgumentParser(description='Train a network')
 	arg_parser.add_argument('--train_seed', type=int, default=0)
-	arg_parser.add_argument('--dataset', type=str, default='cifar10')
-	arg_parser.add_argument('--model', type=str, default='CIFAR10Net')
+	arg_parser.add_argument('--dataset', type=str, default='binsizemnist')
+	arg_parser.add_argument('--model', type=str, default='BinMNISTNET')
 	arg_parser.add_argument('--batch_size', type=int, default=32)
 	arg_parser.add_argument('--optim', type=str, default='sgd', choices=['sgd', 'adam'])
 	arg_parser.add_argument('--epochs', type=int, default=20)
