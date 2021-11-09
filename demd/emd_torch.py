bugfix part 1 for different dualobj values. Everything matches now except gradient=/=dual. both optimize well though

demd/emd_torch.py
@@ -31,7 +31,7 @@ def OBJ(i):
 	# return 0 if max(i) == min(i) else 1
 
 class dEMD(nn.Module):
-	def __init__(self, cost=OBJ, computeDual=True, verbose=False):
+	def __init__(self, cost=OBJ, computeDual=False, verbose=False):
 		super().__init__()
 
 		self.cost = cost
@@ -58,8 +58,7 @@ class dEMD(nn.Module):
 		while all([i < n for i in idx]):
 
 			vals = [AA[i,j] for i,j in zip(range(d), idx)]
-
-			minval = min(vals)#.clone()
+			minval = min(vals).clone()
 			ind = vals.index(minval)
 			xx[tuple(idx)] = minval
 			obj += (OBJ(idx)) * minval
@@ -77,10 +76,11 @@ class dEMD(nn.Module):
 				try: dual[_][i:] = dual[_][i]
 				except: pass
 
-			dualobj =  sum([_.dot(_d) for _, _d in zip(AA, dual)])
+			dualobj =  sum([_.dot(_d) for _, _d in zip(x, dual)])
 
-		# import pdb; pdb.set_trace()
-		return obj, dualobj
+			return obj, dualobj
+		else:
+			return obj
 
 
 
@@ -110,7 +110,7 @@ class dEMDLoss(torch.autograd.Function):
 
 			vals = [AA[i,j] for i,j in zip(range(d), idx)]
 
-			minval = min(vals)#.clone()
+			minval = min(vals).clone()
 			ind = vals.index(minval)
 			xx[tuple(idx)] = minval
 			obj += (OBJ(idx)) * minval
@@ -124,11 +124,11 @@ class dEMDLoss(torch.autograd.Function):
 			try: dual[_][i:] = dual[_][i]
 			except: pass
 
-		dualobj =  sum([_.dot(_d) for _, _d in zip(AA, dual)])
+		dualobj =  sum([_.dot(_d) for _, _d in zip(x, dual)])
 
 		ctx.save_for_backward(dual)
 
-		return obj, dualobj
+		return obj
 
 	@staticmethod
 	def backward(ctx, grad_output):

demd/emd_vanilla.py
@@ -5,18 +5,18 @@ from scipy.optimize import approx_fprime
 
 from .emd import greedy_primal_dual
 
-def matricize(x, n, d):
-    return np.reshape(x, (n, d))
+def matricize(x, d, n):
+	return np.reshape(x, (d, n))
 
 def listify(x):
-    tmp = []
-    for i in range(x.shape[0]):
-        tmp.append(x[i,:])
+	tmp = []
+	for i in range(x.shape[0]):
+		tmp.append(x[i,:])
 
-    return tmp
+	return tmp
 
 def approxGrad(f, x, d, n):
-	grad = approx_fprime(x, f, 1e-8, d, n)
+	grad = np.round(approx_fprime(x, f, 1e-8, d, n))
 	return grad
 
 def demd_func(x, d, n, return_dual_vars=False):
@@ -25,7 +25,7 @@ def demd_func(x, d, n, return_dual_vars=False):
 	log = greedy_primal_dual(x)
 
 	if return_dual_vars:
-		return log['primal objective'], log['dual'], log['dual objective']
+		return log['primal objective'], matricize(log['dual'], d, n), log['dual objective']
 	else:
 		return log['primal objective']
 	

demd/torch_module_emd.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 import time
 
-from emd_torch import dEMD
+from emd_torch import dEMD, dEMDLossFunc
 
 def minimize(func, x_0, niters=100, lr=0.1, verbose=False):
 
@@ -69,16 +69,12 @@ if __name__ == "__main__":
     d = len(data)
     print(data)
 
-    # data = np.array(data)
-    # data = vectorize(data, vecsize)
+    np_data = np.array(data)
 
-    ta1 = torch.Tensor(a1)
-    ta2 = torch.Tensor(a2)
-    ta3 = torch.Tensor(a3)
-    torch_data = [ta1, ta2, ta3]
-    torch_data = torch.stack(torch_data).clone().requires_grad_(requires_grad=True)
+    torch_data = torch.from_numpy(np.array(np_data)).clone().requires_grad_(requires_grad=True)
 
-    func = dEMD()
-    minimize(func, torch_data, niters=4000, lr=0.0001, verbose=True)
+    # func = dEMD()
+    func = dEMDLossFunc
+    minimize(func, torch_data, niters=1000, lr=0.001, verbose=True)
 
    

speed_test.py
@@ -13,7 +13,7 @@ from scipy.optimize import approx_fprime
 import torch
 
 from demd.emd import greedy_primal_dual
-from demd.emd_vanilla import demd_func, approxGrad
+from demd.emd_vanilla import demd_func, approxGrad, matricize
 # from demd.demdLayer import DEMDLayer
 from demd.emd_torch import dEMD, dEMDLossFunc
 
@@ -41,17 +41,21 @@ def test(n, d, seed, gradType, outfile):
 	tmp['gradType'] = [gradType]
 
 	manual_seed(seed)
-	np_data = genNumpyData(n, d)
+
+	if args.dataType == 'N52D':
+		np_data = np.array([[0.1, 0.1, 0.2, 0.5, 0.1], 
+							[0.2, 0.1, 0.3, 0.1, 0.3]])
+	else:
+		np_data = np.array(genNumpyData(n, d))
 
 
 	if gradType == 'scipy' or gradType == 'npdual':
-		np_data = np.array(np_data)
 		d,n = np_data.shape
 		x = np.reshape(np_data, d*n)
 
 	elif gradType == 'torchdual' or gradType == 'autograd':
 		# torch stuff
-		x = torch.from_numpy(np.array(np_data)).clone().requires_grad_(requires_grad=True)
+		x = torch.from_numpy(np_data).clone().requires_grad_(requires_grad=True)
 
 	else:
 		print(f'Unknown GradType: {gradType}')
@@ -60,11 +64,11 @@ def test(n, d, seed, gradType, outfile):
 	t1 = time.time()
 
 	if gradType == 'scipy':
-		_ = demd_func(x, d, n, return_dual_vars=False)
+		funcval, _, dualobj = demd_func(x, d, n, return_dual_vars=True)
 
 		t2 = time.time()
 
-		grad = approxGrad(demd_func, x, d, n)
+		grad = matricize(approxGrad(demd_func, x, d, n), d, n)
 
 	elif gradType == 'npdual':
 		funcval, grad, dualobj = demd_func(x, d, n, return_dual_vars=True)
@@ -72,7 +76,7 @@ def test(n, d, seed, gradType, outfile):
 		t2 = time.time() # gradient already computed from dual
 
 	elif gradType == 'torchdual':
-		funcval, dualobj = dEMDLossFunc(x)
+		funcval = dEMDLossFunc(x)
 		t2 = time.time()
 
 		funcval.backward()
@@ -130,6 +134,9 @@ if __name__ == "__main__":
 						choices=['scipy', 'npdual', 'torchdual', 'autograd'])
 
 	arg_parser.add_argument('--outfile', type=str, default='results/speed_test_results.csv')
+
+	arg_parser.add_argument('--dataType', type=str, default='gausspseudo',
+						choices=['gausspseudo', 'N52D'])
 
 	args = arg_parser.parse_args()
 	main(args)