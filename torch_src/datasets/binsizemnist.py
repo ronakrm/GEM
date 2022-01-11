b2416c7206e43603219e590c751ccf24f8089a25
Ronak Mehta <ronakrm@cs.wisc.edu>
2022-01-11 13:37:55 -0600

    added save/load for binary small/big mnist

--- torch_src/datasets/binsizemnist.py
+++ torch_src/datasets/binsizemnist.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 import numpy as np
 import torch
 
+import h5py
 
 class BinarySizeMNIST(MNIST):
     SIZE = 64
@@ -33,15 +34,37 @@ class BinarySizeMNIST(MNIST):
 
         if self.train:
             data_file = self.training_file
+            bindatafile = root + '/' + type(self).__name__ + '/train.h5'
         else:
             data_file = self.test_file
+            bindatafile = root + '/' + type(self).__name__ + '/valid.h5'
+        
         # self.data, self.targets = torch.load(
             # Path(self.root, self.processed_folder, data_file))
 
-        self._build()
+        if Path(bindatafile).exists():
+            try:
+                myh5 = h5py.File(bindatafile, 'r')
+                self.data = np.array(myh5['data'])
+                self.targets = np.array(myh5['targets'])
+                self.attrs = np.array(myh5['attrs'])
+
+                print('Loaded dataset...')
+            except:
+                print('*'*70)
+                print('Possibly Corrupted Data, delete files and rerun to regenerate.')
+                print('*'*70)
+
+        else:
+            self._build()
+            hf = h5py.File(bindatafile, 'w')
+            hf.create_dataset('data', data=self.data)
+            hf.create_dataset('targets', data=self.targets)
+            hf.create_dataset('attrs', data=self.attrs)
+            hf.close()
 
     def _build(self):
-        data_, targets_ = [], []
+        data_, targets_, attrs_ = [], [], []
 
         print("Building dataset...")
         for [img, target] in tqdm(zip(self.data, self.targets)):
@@ -52,19 +75,22 @@ class BinarySizeMNIST(MNIST):
             small[self.SMALL_START:self.SMALL_END, self.SMALL_START:self.
                   SMALL_END] += img
             data_.append(small)
-            targets_.append((target, 0))
+            targets_.append(target)
+            attrs_.append(0)
 
             # "big" versions of each image in the MNIST dataset
             big = np.array(Image.fromarray(img).resize((self.SIZE, self.SIZE)))
 
             data_.append(big)
-            targets_.append((target, 1))
+            targets_.append(target)
+            attrs_.append(1)
 
         self.data = data_
         self.targets = targets_
+        self.attrs = attrs_
 
     def __getitem__(self, index):
-        img, target = self.data[index], self.targets[index]
+        img, target, attr = self.data[index], self.targets[index], self.attrs[index]
 
         img = Image.fromarray(img)
         if self.transform is not None:
@@ -73,6 +99,6 @@ class BinarySizeMNIST(MNIST):
             img = transforms.ToTensor()(img)
 
         # (class, big_or_small, index)
-        target = torch.tensor([int(target[0]), int(target[1]), index])
+        target = torch.tensor([int(target), int(attr), index])
 
         return img, target
