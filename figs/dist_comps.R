6d62c939677e3b684d260da9aa1923ce3cda02a8
Ronak Mehta <ronakrm@cs.wisc.edu>
2022-04-26 10:51:41 -0500

    more figs for demd vs bary

--- figs/dist_comps.R
+++ figs/dist_comps.R
@@ -27,16 +27,17 @@ myTheme = theme(panel.background = element_rect(fill = NA, color = "black"),
 )
 
 ####speed histogram
-othistdata = data.frame(read.csv("results/wassimplresults.csv"))
+#othistdata = data.frame(read.csv("results/wassimplresults.csv"))
+othistdata = data.frame(read.csv("results/wassimplresults_big.csv"))
 #othistdata = data.frame(read.csv("results/ot_1d_results.csv"))
 
 #speeddata = subset(speedhistdata, gradType!='torchdual')
 #speeddata = subset(speeddata, gradType!='autograd')
 #speeddata = subset(speedhistdata, gradType!='npdual')
 #speeddata = subset(speeddata, gradType!='scipy')
-othistdata = subset(othistdata, n=5)
-#othistdata = subset(othistdata, n=2)
-#othistdata = subset(othistdata, d<10)
+#othistdata = subset(othistdata, n==100)
+#othistdata = subset(othistdata, n==2)
+othistdata = subset(othistdata, nbins==10)
 
 dorder = c("2", "5", "10", "20", "50", "100")
 othistdata$d <- factor(as.character(othistdata$d), levels = dorder)
@@ -55,4 +56,58 @@ gg <- ggplot(othistdata, aes(x=d, y=time_per_epoch, fill=distType)) +
   scale_fill_discrete(labels = c("EMD", "PairwiseWass"))
   #guides(fill="none", alpha="none")
 gg
-ggsave("results/Distance_Comparisons.pdf", height = 4, width = 6, units = "in")
+ggsave("results/wasbary_comparisons_1.pdf", height = 4, width = 6, units = "in")
+
+
+dorder = c("2", "5", "10", "20", "50", "100")
+othistdata$d <- factor(as.character(othistdata$d), levels = dorder)
+#torder = c("demd", "sink_1d_bary", "lp_1d_bary", "cvx")
+torder = c("demd", "pairwass")
+othistdata$model <- factor(as.character(othistdata$model), levels = torder)
+
+gg <- ggplot(othistdata, aes(x=d, y=loss, fill=distType)) + 
+  geom_boxplot(alpha=1.0) +
+  myTheme + 
+  #theme(legend.position = c(0.15,0.7)) +
+  theme(legend.position="bottom", legend.title = element_blank()) + 
+  ylab('Loss (Distance)') + 
+  xlab('Number of Distributions') +
+  #scale_y_continuous(trans='log2', labels=scaleFUN) +
+  scale_fill_discrete(labels = c("EMD", "PairwiseWass"))
+#guides(fill="none", alpha="none")
+gg
+ggsave("results/wasbary_comparisons_1.pdf", height = 4, width = 6, units = "in")
+
+
+
+
+#############
+
+
+
+####speed histogram
+lossdata = data.frame(read.csv("results/2_tmpres.csv"))
+
+dorder = c("2", "5", "10", "20", "50", "100")
+othistdata$d <- factor(as.character(othistdata$d), levels = dorder)
+#torder = c("demd", "sink_1d_bary", "lp_1d_bary", "cvx")
+torder = c("demd", "pairwass")
+othistdata$model <- factor(as.character(othistdata$model), levels = torder)
+
+gg <- ggplot(othistdata, aes(x=d, y=time_per_epoch, fill=distType)) + 
+  geom_boxplot(alpha=1.0) +
+  myTheme + 
+  #theme(legend.position = c(0.15,0.7)) +
+  theme(legend.position="bottom", legend.title = element_blank()) + 
+  ylab('Time Per Epoch (seconds)') + 
+  xlab('Number of Distributions') +
+  #scale_y_continuous(trans='log2', labels=scaleFUN) +
+  scale_fill_discrete(labels = c("EMD", "PairwiseWass"))
+#guides(fill="none", alpha="none")
+gg
+ggsave("results/wasbary.pdf", height = 4, width = 6, units = "in")
+
+
+
+
+
